d6b2c6a4efb208556b8e940877041f6848a9bae6
patrykharasiuk <patryk.harasiuk@op.pl>
2024-04-25 23:38:14 +0200

    feat: add static gradient box

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="gradient-heading">Gradient generator</h1>
-    </>
+    <div className="wrapper">
+      <div className="heading-wrapper">
+        <h1 className="gradient-heading">Gradient Generator</h1>
+        <p className="paragraph">Beautiful, luscious gradients ✨</p>
+      </div>
+
+      <div className="gradient-box-background">
+        <div className="gradient-box-wrapper">
+          <div className="gradient-box" />
+        </div>
+      </div>
+    </div>
   );
 }
 
